9755df2b1e7118729c2c9531434aa59c9ef336ca
ura148 <yuki.ura10@gmail.com>
2020-04-09 14:44:20 +0900

    Bottleが動かないように設定。 ただその影響により背景のparallaxの動きがおかしくなる。

--- js/style.js
+++ js/style.js
@@ -5,7 +5,6 @@ $(document).ready(function(){
 
       $("#background-effect").css("height",contensHeight);
       $(".background-effect__img").css("width",windowW * 4);
-
 });
 
 
@@ -21,13 +20,21 @@ $(function(){
         targetFactor = 0.2,//targetFactorという変数に0.5を代入
         windowH = $(window).height();//window（使用している端末で表示される画面の高さ）
         //
-        bgebStart1 = bgebPosOT - windowH,//背面遅延1のY座標-画面の高さ
+        bgebStart1 = bgebPosOT - windowH,//Y座標-画面の高さ
         eyecatchStart = eyecatchPosOt - windowH,
         eyecatchPdStart = eyecatchPdPosOt - windowH,
+        //
+
+
+
 
   $(window).on('scroll',function(){
     let scrollY = $(this).scrollTop(),//scrollする度のscrolltopのY座標
+
         toggle = (scrollY - bgebPosOT) * targetFactor
+        toggle2 = (scrollY - eyecatchPdPosOt)
+
+
 
           if(scrollY > bgebStart1){
             //"="になる時はscrollbottomと背面遅延1のY座標が同じ時、1pxスクロールすれば背面遅延が画面内に入る
@@ -35,15 +42,24 @@ $(function(){
 
           }else{
 
+          }if(scrollY > eyecatchPdStart){
+            eyecatchPd.css('transform', 'translateY(' + toggle2 + 'px)'),
+            eyecatchPd.css('margin-top', 0 + eyecatchPdPosOt),
+            eyecatch.css('padding-bottom', 144 - eyecatchPdPosOt);
+
+          }else{
+            eyecatchPd.css('background-position','center top');
+            eyecatch.css('padding-bottom', 144);
+
           }if(scrollY > eyecatchStart){
             //"="になる時はscrollbottomと背面遅延1のY座標が同じ時、1pxスクロールすれば背面遅延が画面内に入る
             eyecatch.css('background-position-y', (scrollY - eyecatchPosOt) + 'px');
+
 
           }else{
             //そうでない時は
             eyecatch.css('background-position','center top');
             //eyecatchのpositionを通常に戻す
-
           }
   });
 });
