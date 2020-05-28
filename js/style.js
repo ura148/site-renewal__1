de082661f93831b22911cb40274fcb9d3e1a94e8
ura148 <yuki.ura10@gmail.com>
2020-05-29 00:50:23 +0900

    fix： ・jQueryの記述を整理 ・気泡のpositionを変更

--- js/style.js
+++ js/style.js
@@ -1,163 +1,139 @@
-// ==============================
+let deviceW = 767;
+    main = $("#main");
+
+
+$(window).on({
+  "load":function(){
+;
+  },
+  "load resize":function(){
+    loadingAnimation();
+    reasonAreaSet();
+    eyecatchSet();
+    bubbleSize();
+    productArea();
+
+  },
+  "scroll":function(){
+    bubbleMove();
+  }
+});
+
+
 // 初期処理が終わるまでのアニメーション
-//
-    window.onload = () => {
-    // ページのコンテンツを全て読み込み終わった後に実行する処理
+let loadingAnimation = function(){
+  let animation = document.getElementById('loading-animation');
 
-    let animation = document.getElementById('loading-animation');
-    // spinner という箱に #loading-animation を入れる
+  animation.classList.add('loaded');
+  }
 
-    animation.classList.add('loaded');
-    // animation に対してclass loaded を追加する
-    };
+// 泡のサイズ変更
+let bubbleSize = function(){
+  let windowW = $(window).width();
+      windowH = $(window).height();
+  if(windowW <= deviceW){
+    $(".background-effect__img").css("width",windowW * 3);
+  }else{
+    $(".background-effect__img").css("width",windowW);
+  }
+}
 
-// ==============================
-// 初期処理
-//
-$(window).on('load resize scroll',function(){
-  let contentsBox = $("#main-container"),
+// bgeの気泡の動き
+let bubbleMove = function(){
+  let bgeb = $("#background-effect__box"),
+      bgebPosOT = bgeb.offset().top;
+      targetFactor = 0.2;
+      windowH = $(window).height();
+      bgebStart1 = bgebPosOT - windowH;
+      scrollY = $(this).scrollTop();
+      toggle = (scrollY - bgebPosOT) * targetFactor ;
+      bgeb.css('transform', 'translateY(' + - toggle + 'px)');
+  }
+
+// eyecatch areaの調整
+let eyecatchSet = function(){
+  let windowW = $(window).width();
+      windowH = $(window).height();
       eyecatchBox = $(".eyecatch-box");
-      eyecatchImgSp = $("#eyecatch-img__sp")
-      contensHeight =contentsBox.height(),
-      windowW = $(window).width(),
-      windowH = $(window).height(),
-      deviceW = 768,
-      main = $("#main"),
-      // reason areaについての取得情報
-      reasontextArea = $("#reason-list"),
-      reasontextLast = $("#reason-text-last"),
-      reasonImgArea = $("#reason-product"),
-      reasonflex = $("#reason-flex"),
-      eyeCatchPosTO = eyecatchBox.offset().top,
-      reasontextAreaPosTO = reasontextArea.offset().top,
-      reasontextLastPosTO = reasontextLast.offset().top,
-      reasonflexW = reasonflex.width(),
-      reasonImgAreaW = reasonImgArea.width(),
+      eyecatchImgSp = $("#eyecatch-img__sp");
+      eyecatchImgSpTO = eyecatchImgSp.offset().top;
+      eyeCatchPosTO = eyecatchBox.offset().top;
       eyecatchImgSpH = eyecatchImgSp.height();
-      reasontextLastH = reasontextLast.height(),
-      reasontextLastBottom = reasontextLastPosTO + reasontextLastH,
-      reasontextAreaH = reasontextLastBottom - reasontextAreaPosTO;
+      // yecatchImgSpMaxH = 100vh - eyecatchImgSpTO;
       eyecatchBoxH = windowH - eyeCatchPosTO;
-      eyecatchBoxHsp = windowH - eyeCatchPosTO - eyecatchImgSpH;
+      eyecatchBoxHsp = windowH - eyecatchImgSpH;
 
+      console.log(eyecatchImgSpH);
+      console.log(windowH);
 
-      main.css("height",contensHeight);
+      if(windowW <= deviceW){
+        $(".eyecatch").css("padding-bottom",eyecatchBoxHsp);
+        console.log("sp");
+      }else{
+        $(".eyecatch").css("padding-bottom",eyecatchBoxH);
+        $("#eyecatch-img__pc").css("height",eyecatchBoxH);
+        console.log("pc");
+      }
+}
 
-      $("#background-effect").css("height",contensHeight);
-        if(windowW <= deviceW){
-        // 768px以下の時の処理
-        // ==============================
-        // 気泡の画像サイズを調整
-        //
-        $(".background-effect__img").css("width",windowW * 4);
-        // ==============================
-        // eyecatchエリアサイズ調整
-        $(".eyecatch").css("padding-bottom",eyecatchBoxHsp),
+// reason areaのサイズ調整
+let reasonAreaSet = function(){
+  let windowW = $(window).width();
+      reasontextArea = $("#reason-list");
+      reasontextLast = $("#reason-text-last");
+      reasonImgArea = $("#reason-product");
+      reasonflex = $("#reason-flex");
+      reasontextAreaPosTO = reasontextArea.offset().top;
+      reasontextLastPosTO = reasontextLast.offset().top;
+      reasonflexW = reasonflex.width();
+      reasonImgAreaW = reasonImgArea.width();
+      reasontextLastH = reasontextLast.height();
+      reasontextLastBottom = reasontextLastPosTO + reasontextLastH;
+      reasontextAreaH = reasontextLastBottom - reasontextAreaPosTO;
 
-        // ==============================
-        // reasonエリアのサイズ調整
-        //
-        $(".reason-product__img").css("height",reasontextAreaH),
-        reasonflex.css("height",reasontextAreaH),
-        reasontextArea.css("height",reasontextAreaH),
+      if(windowW <= deviceW){
+        reasonflex.css("height",reasontextAreaH);
+        reasontextArea.css("height",reasontextAreaH);
         reasonImgArea.css('transform', 'translateY( 0 )');
-
-        // ==============================
-        // productエリアのサイズ調整
-        //
-        $("#ProdInfo-text-area").css("height","auto"),
-        $("#Product__img").css("height","auto");
+        $(".reason-product__img").css("height",reasontextAreaH);
 
       }else{
-        // 768pxより大きい時の処理
-
-        let
-            productTextArea = $("#ProdInfo-text-area"),
-            productText1st = $("#ProdInfo-txt"),
-            productBtn = $("#ProdInfo__btn"),
-            productImg = $("#Product__img"),
-            productFlex = $("#Product-flex"),
-            productFlexW = productFlex.width(),
-            productImgW = productImg.width(),
-            productBtnH = productBtn.outerHeight(),
-            eyeCatchPosTO = eyecatchBox.offset().top,
-            productText1stPosTO = productText1st.offset().top,
-            productBtnPosTO = productBtn.offset().top,
-            productBtnPosBottom = productBtnPosTO + productBtnH,
-            productTextAreaH = productBtnPosBottom - productText1stPosTO;
-
-
-        // ==============================
-        //気泡の画像サイズを調整する
-        //
-        $(".background-effect__img").css("width",windowW);
-        //
-        //
-        // ===============================
-
-        // ==============================
-        //eyecatchの画像サイズ変更
-        //
-        $(".eyecatch").css("padding-bottom",eyecatchBoxH),
-        $("#eyecatch-img__pc").css("height",eyecatchBoxH);
-
-
-
-        // ==============================
-        // reasonの画像サイズを調整する
-        //
-        reasontextArea.css("height",reasontextAreaH),
-        reasonflex.css("height",reasontextAreaH),
-        $(".reason-product__img").css("height",reasontextAreaH * 1.1961),
+        reasonflex.css("height",reasontextAreaH);
+        reasontextArea.css("height",reasontextAreaH);
         reasonImgArea.css('transform', 'translateY(' + - reasontextAreaH * 0.1961 + 'px)');
         reasontextArea.css("width",reasonflexW - reasonImgAreaW);
-        //
-        //
-        // ===============================
-
-
-        // ==============================
-        // productエリアの調整
-        //
-        $("#ProdInfo-text-area").css("height",productTextAreaH),
-        productImg.css("height",productTextAreaH);
-
-        //
-        //
-        // ===============================
+        $(".reason-product__img").css("height",reasontextAreaH * 1.1961);
       }
-});
-//
-//
-// ===============================
+}
 
-// ==============================
-// bgeの気泡の動き
-//
-$(function(){
-    let bgeb = $("#background-effect__box"),
-        bgebPosOT = bgeb.offset().top,
-        targetFactor = 0.2,
-        windowH = $(window).height();
-        bgebStart1 = bgebPosOT - windowH,
+let productArea = function(){
+  let windowW = $(window).width();
+      productTextArea = $("#ProdInfo-text-area");
+      productImg = $("#Product__img");
 
-        $(window).on('scroll',function(){
-          let scrollY = $(this).scrollTop(),//scrollする度のscrolltopのY座標
-              toggle = (scrollY - bgebPosOT) * targetFactor ;
+  if(windowW <= deviceW){
+    $("#ProdInfo-text-area").css("height","auto");
+    $("#Product__img").css("height","auto");
 
-              bgeb.css('transform', 'translateY(' + - toggle + 'px)');
-        });
-});
-//
-//
-// ===============================
+  }else{
+  let productText1st = $("#ProdInfo-txt");
+      productBtn = $("#ProdInfo__btn");
+      productFlex = $("#Product-flex");
+      productFlexW = productFlex.width();
+      productImgW = productImg.width();
+      productBtnH = productBtn.outerHeight();
+      productText1stPosTO = productText1st.offset().top;
+      productBtnPosTO = productBtn.offset().top;
+      productBtnPosBottom = productBtnPosTO + productBtnH;
+      productTextAreaH = productBtnPosBottom - productText1stPosTO;
 
-// ==============================
+    productImg.css("height",productTextAreaH);
+  }
+}
+
 // メニューボタンの設定
-//
 $(".btn-trigger").on("click",function(){
   $(this).toggleClass("active");
-  // $(".header-nav").toggleClass("dn");
   $(".header-nav").fadeToggle(500);
 });
 
@@ -165,14 +141,9 @@ $(".nav-link").on("click",function(){
   $(".btn-trigger").toggleClass("active");
   $(".header-nav").fadeToggle(500);
 });
-//
-//
-// ===============================
 
 
-// ===============================
 // ページ内リンクの設定
-//
 $('a[href^="#"]').click(function() {
   // スクロールの速度
   let adjust = 50;
@@ -186,6 +157,3 @@ $('a[href^="#"]').click(function() {
   }, speed, 'swing');
   return false;
 });
-//
-//
-// ===============================
